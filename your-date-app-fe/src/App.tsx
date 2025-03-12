40ef9445230677a30c32aaf930f21bc0ebed0d83
maarek94 <maarekgrzeska@gmail.com>
2025-03-13 00:32:20 +0100

    added InsideAppLayout and dashboardpage

--- your-date-app-fe/src/App.tsx
+++ your-date-app-fe/src/App.tsx
@@ -1,9 +1,11 @@
 import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import AppLayout from "./components/AppLayout/AppLayout";
+import InsideAppLayout from "./components/AppLayout/InsideAppLayout";
 import Homepage from "./pages/Homepage/Homepage";
 import Loginpage from "./pages/Loginpage/Loginpage";
 import Registerpage from "./pages/Registerpage/Registerpage";
+import Dashboardpage from "./pages/Dashboardpage/Dashboardpage";
 
 const App: React.FC = () => {
   return (
@@ -14,6 +16,9 @@ const App: React.FC = () => {
             <Route index element={<Homepage />} />
             <Route path="login" element={<Loginpage />} />
             <Route path="register" element={<Registerpage />} />
+          </Route>
+          <Route element={<InsideAppLayout />}>
+            <Route path="board" element={<Dashboardpage />} />
           </Route>
         </Routes>
       </BrowserRouter>
---
+++ your-date-app-fe/src/components/AppLayout/InsideAppLayout.tsx
@@ -0,0 +1,19 @@
+import { Outlet } from "react-router-dom";
+import Header from "../Header/Header";
+import Footer from "../Footer/Footer";
+import React from "react";
+import "./AppLayout.css";
+
+const InsideAppLayout: React.FC = () => {
+  return (
+    <div className="app-container">
+      <Header />
+      <div className="content-container">
+        <Outlet />
+      </div>
+      <Footer />
+    </div>
+  );
+};
+
+export default InsideAppLayout;
